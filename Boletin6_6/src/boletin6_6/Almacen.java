53c510823317bc59c35bdaa6f7892370783e8e1d
jmartinezpineiro <jmartinezpineiro@nautilus16.danielcastelao.org>
2018-10-31 10:32:06 +0100

    Finalizado

---
+++ Boletin6_6/src/boletin6_6/Almacen.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Juan Martínez
+ */
+public class Almacen {
+    public void ArticuloConsumo(){
+        int ventas;
+       
+        ventas = Integer.parseInt(JOptionPane.showInputDialog("Introduzca cantidad de articulos:"));
+        if (ventas>1000){
+       JOptionPane.showMessageDialog(null, "primera necesidad");
+        }
+        else if(ventas > 500&& ventas <=500){
+       JOptionPane.showMessageDialog(null, "alto");
+        }
+         else if(ventas > 100&& ventas <=500){
+        JOptionPane.showMessageDialog(null, "medio");
+        }
+         else {
+        JOptionPane.showMessageDialog(null, "bajo");
+         }
+        
+    }
+}
---
+++ Boletin6_6/src/boletin6_6/Boletin6_6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Juan Martínez
+ */
+public class Boletin6_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Almacen venta = new Almacen ();
+        
+        //METODO
+        venta.ArticuloConsumo();
+    }
+    
+}
